8e6cb71f9f30397a889ca2159cdef18d9d2edad5
pradathrashmika337-ui <pradathrashmika337@gmail.com>
2025-11-15 11:53:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Tr2mZQ'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?mOhkdi'
 };
